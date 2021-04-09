0621e2e6116b24a0673f1d5021dd5a3e34f80f7c
ikumatoshihiro <arsmagna912@gmail.com>
2021-04-09 20:19:40 +0900

    掲示板

---
+++ 4each_keijiban/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<meta charset="UTF-8">
+<title>4eachblog 掲示板</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+
+    <body>
+        <?php
+        
+        mb_internal_encoding("utf8");
+        $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+        $stmt = $pdo->query("select * from 4each_keijiban");
+        
+        while ($row = $stmt->fetch()) {
+            echo $row['handlename'];
+            echo $row['title'];
+            echo $row['comments'];
+        }
+        
+        
+        
+        ?>
+        
+        <div class="logo"><img src="4eachblog_logo.jpg"></div>
+    <header>
+        <ul>
+            <li>トップ</li>
+            <li>プロフィール</li>
+            <li>4eachについて</li>
+            <li>登録フォーム</li>
+            <li>問い合わせ</li>
+            <li>その他</li>
+        </ul>
+    </header>
+        
+        <main>
+            <div class="main-container">
+                <div class="left">
+                    <h1>プログラミングに役立つ掲示板</h1>
+                    <form method="post" action="insert.php">
+                        <div class="abc"><h3>入力フォーム</h3></div>
+                        <div>
+                            <label>ハンドルネーム</label>
+                            <br>
+                            <input type="text" class="text" size="25" name="handlename">
+                        </div>
+                        
+                        <div>
+                            <label>タイトル</label>
+                            <br>
+                            <input type="text" class="text" size="25" name="title">
+                        </div>
+                        <div>
+                            <label>コメント</label>
+                            <br>
+                            <textarea cols="50" rows="7" name="comments"></textarea>
+                        </div>
+                        
+                        <div>
+                            <input type="submit" class="submit" value="投稿する">
+                        </div>
+                    </form>
+                    
+                    
+                <?php
+                    echo"<div class='def'>";
+                    echo"<h3>タイトル</h3>";
+                    "<div class='contents'>";
+                    echo"記事の中身。記事の中身。記事の中身。記事の中身。記事の中身。記事の中身。記事の中身。
+                    記事の中身。記事の中身。記事の中身。記事の中身。記事の中身。<br>
+                    記事の中身。記事の中身。記事の中身。記事の中身。記事の中身。記事の中身。<br>
+                    記事の中身。記事の中身。記事の中身。記事の中身。記事の中身。記事の中身。<br>";
+                    echo"<div class='handlename'>posted by 通りすがり</div>";
+                    echo"</div>";
+                    echo"<br>";
+                    
+                    while ($row = $stmt->fetch()) {
+                        echo "<div class='def'>";
+                        echo "<h3>".$row['title']."</h3>";
+                        echo "<div class='contents'>";
+                        echo $row['comments'];
+                        echo "<div class='handlename'>posted by".$row['handlename']."<div>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                ?>
+                    
+                    
+                    <div class="ghi"><h3>タイトル</h3></div>
+                    
+                
+                </div>
+                
+                </div>
+                
+                
+                <div class="right">
+                    <h2>人気の記事</h2>
+                    <ul>
+                        <li>PHPおすすめ本</li>
+                        <li>PHP MyAdminの使い方</li>
+                        <li>今人気のエディタ Top5</li>
+                        <li>HTMLの基礎</li>
+                    </ul>
+                    
+                    <h2>オススメリンク</h2>
+                    <ul>
+                        <li>インターノウス株式会社</li>
+                        <li>XAMPPのダウンロード</li>
+                        <li>Eclipsのダウンロード</li>
+                        <li>Bracketsのダウンロード</li>
+                    </ul>
+            
+                    <h2>カテゴリ</h2>
+                    <ul>
+                        <li>HTML</li>
+                        <li>PHP</li>
+                        <li>MySQL</li>
+                        <li>JavaScript</li>
+                    </ul>
+            </div>
+            
+            
+            <footer>
+                copyright ©︎ internous|4each blog the which provides AtoZ about programing.
+            </footer>
+        
+        </main>
+    
+    </body>
+    
+</html>
